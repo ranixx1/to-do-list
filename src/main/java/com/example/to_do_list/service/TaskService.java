ab7b52c8adf94ca0907cb3087c2099e4ef81da1b
ranilton <ranilton.c@escolar.ifrn.edu.br>
2025-05-05 10:11:11 -0300

    logica de negócios

---
+++ src/main/java/com/example/to_do_list/service/TaskService.java
@@ -0,0 +1,40 @@
+package com.example.to_do_list.service;
+
+import com.example.to_do_list.model.Task;
+import com.example.to_do_list.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.time.LocalDateTime;
+
+import java.util.List;
+
+@Service
+public class TaskService {
+    private final TaskRepository taskRepository;
+
+    @Autowired
+    public TaskService(TaskRepository taskRepository){
+        this.taskRepository = taskRepository;
+    }
+
+    public List<Task> findAll(){
+        return taskRepository.findAll();
+    }
+    
+    public Task save(Task task){
+        return taskRepository.save(task);
+    }
+
+    public void deleteById(Long id){
+        taskRepository.deleteById(id);
+    }
+    
+    public Task toggleTaskCompletion(Long id) {
+        return taskRepository.findById(id).map(task -> {
+            boolean currentStatus = task.isCompleted();
+            task.setCompleted(!currentStatus);
+            task.setUpdatedAt(LocalDateTime.now());
+            return taskRepository.save(task);
+        }).orElseThrow(() -> new RuntimeException("Task not found"));
+    }
+}
